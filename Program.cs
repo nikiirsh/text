a3c7b62b7950ecfb9b64a13c8e56825d329e1300
nikiirsh <87150515+nikiirsh@users.noreply.github.com>
2022-10-27 00:40:28 +0300

    exceptions

--- Program.cs
+++ Program.cs
@@ -4,19 +4,48 @@
     {
         static void Main(string[] args)
         {
-            var path = "D:\\diskC\\учеба\\lessons\\simple code\\hwtext\\phonebook.csv";
-            var phonebook = ReadFile(ref path);
-            Console.WriteLine("Adding new:");
-            AddPerson(path);
-            Console.WriteLine("Searching person");
-            var person = SearchPerson(Console.ReadLine(), phonebook);
-            Console.WriteLine(person);
+            try
+            {
+                var path = "D:\\diskC\\учеба\\lessons\\simple code\\hwtext\\phonebook.csv";
+                var phonebook = ReadFile(ref path);
+                Console.WriteLine("Adding new:");
+                AddPerson(path);
+                Console.WriteLine("Searching person");
+                var person = SearchPerson(Console.ReadLine(), phonebook);
+                Console.WriteLine(person);
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("Check if the file path is correct");
+            }
+            
         }
 
         static List<(string FirstName, string LastName, string PhoneNumber)> ReadFile(ref string path)
         {
+
+            //try
+            //{
+            //string.Equals("123", "244", StringComparison.InvariantCultureIgnoreCase);  
+            //var book = new List<(string FirstName, string LastName, string PhoneNumber)>();//не хочет использовать book and lines в try/catch
+            //var lines = File.ReadAllLines(path);
+            //foreach (var line in lines)
+            //{
+            //    var split = line.Split(',');
+            //    book.Add((split[0], split[1], split[2]));
+            //}
+
+            //return book;
+
+            //}
+            //catch
+            //{
+            //    return null;
+            //} 
+
             string.Equals("123", "244", StringComparison.InvariantCultureIgnoreCase);
-            if (!File.Exists(path)) return null;
+            if (!File.Exists(path)) 
+                throw new ArgumentNullException("File is a null");
             var book = new List<(string FirstName, string LastName, string PhoneNumber)>();
             var lines = File.ReadAllLines(path);
             foreach (var line in lines)
